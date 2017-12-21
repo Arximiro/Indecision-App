db53d7f7dc3edca0d28f7eb33f687a5bbdb948b9
arximiro <arximiro@outlook.com>
2017-12-21 03:55:00 -0500

    Installed transform-class-properties plugin and updated class based components to use it

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,22 +6,35 @@ import Options from './Options';
 
 
 export default class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-
-        this.state = {
-            options: []
-        };
-    }
+    state = {
+        options: []
+    };
+    handleDeleteOptions = () => {
+        this.setState(() => ({options: []}));
+    };
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => optionToRemove !== option)
+        }));
+    };
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+        alert(option)
+    };
+    handleAddOption = (option) => {
+        if(!option) {
+            return 'Enter a valid value';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists';
+        }
+        this.setState((prevState) => ({options: prevState.options.concat(option)}));
+    };
     componentDidMount() {
         try {
             const json = localStorage.getItem('options');
             const options = JSON.parse(json);
-            options ? this.setState(() => ({options})) : undefined;
+            if(options) this.setState(() => ({options}))
         } catch (e) {}        
     }
     componentDidUpdate(prevProps, prevState) {
@@ -30,21 +43,6 @@ export default class IndecisionApp extends React.Component {
             localStorage.setItem('options', json);
         }
     }
-      
-    handleDeleteOptions() {this.setState(() => ({options: []}))}
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option) => optionToRemove !== option)
-        }))
-    }
-    handlePick() {alert(this.state.options[Math.floor(Math.random() * this.state.options.length)])}
-    handleAddOption(option) {
-        if(!option) {
-            return 'Enter a valid value';
-        } else if (this.state.options.indexOf(option) > -1) {
-            return 'This option already exists';
-        }
-        this.setState((prevState) => ({options: prevState.options.concat(option)}))}
     render() {        
         return (
             <div>
