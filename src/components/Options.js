bfff2a45ef235685a395de60bf4ccfe18a45064e
arximiro <arximiro@outlook.com>
2017-12-22 11:40:32 -0500

    Updated options component messages

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,10 +4,19 @@ import Option from './Option'
 const Options = (props) => (
         <div>
         <div className="widget-header">
-          <h3 className="widget-header__title">Your Options</h3>
+          <h3
+            className="widget-header__title"
+            hidden={props.options.length === 0}
+          >
+          Your Options
+          </h3>
           <button 
             className="button button--link"
-            onClick={props.handleDeleteOptions}>Remove All</button>
+            hidden={props.options.length < 2}
+            onClick={props.handleDeleteOptions}
+          >
+          Remove All            
+          </button>
         </div>
           
           {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
